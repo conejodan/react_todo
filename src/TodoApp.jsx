f93b6be92797a695a5eb8bc4f0fd187c71cdb767
Daniel Cornelio <jdcornelio89@gmail.com>
2022-10-30 17:45:02 -0600

    Agregando DefaultProps

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -2,17 +2,24 @@ import PropTypes from "prop-types";
 
 const titulo = "Hola";
 
-export const TodoApp = (props) => {
+export const TodoApp = ({ title, subTitle }) => {
   return (
     <>
       <h1>
-        {titulo} {props.title}
+        {titulo} {title}
       </h1>
-      <p>esto es un parrafo</p>
+      <p>{subTitle}</p>
     </>
   );
 };
 
 TodoApp.propTypes = {
   title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+};
+
+TodoApp.defaultProps = {
+  title: "No hay titulo",
+  subTitle: "Subtitulo",
+  name: "Daniel Cornelio",
 };
